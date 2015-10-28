de9bdb965884fdc6f74d9c1d35d67e86efc28f97
tienn2t <tienn2t@gmail.com>
2015-10-28 21:34:04 +0700

    update

--- protected/controllers/web/StoryController.php
+++ protected/controllers/web/StoryController.php
@@ -19,8 +19,8 @@ class StoryController extends Controller{
         $pager->setPageSize($limit);
 
         $chapters = $chapter->getChapterByStory($table,$slug,50,$pager->getOffset(),'id,chapter_number,chapter_slug,chapter_name');
-
-        $this->render('view',compact('pager','story','chapters'));
+        $storyAuthor = StoryModel::model()->getStoryByAuthor($story->author);
+        $this->render('view',compact('pager','story','chapters','storyAuthor'));
     }
 
     public function actionDetail(){
