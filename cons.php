d0883354074ee9a112aa1508fd954cef03483f66
tienn2t <tienn2t@gmail.com>
2015-10-29 21:24:28 +0700

    update

--- cons.php
+++ cons.php
@@ -3,4 +3,4 @@ define('_APP_PATH_', dirname(__FILE__));
 define('DS', DIRECTORY_SEPARATOR);
 date_default_timezone_set('Asia/Saigon');
 //$yii='yii-1.1.13.e9e4a0/framework/yii.php';
-$yii='/var/www/yii/framework/yii.php';
+$yii='yii/framework/yii.php';
