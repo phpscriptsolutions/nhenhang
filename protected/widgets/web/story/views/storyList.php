9530697fe70131316c36fa2b822ced63260f9424
tienn2t <tienn2t@gmail.com>
2015-10-30 23:32:00 +0700

    update

--- protected/widgets/web/story/views/storyList.php
+++ protected/widgets/web/story/views/storyList.php
@@ -9,33 +9,35 @@
     </div>
     <?php if(!empty($stories) && count($stories)):?>
     <ul>
+        <div class="row">
         <?php foreach($stories as $story):?>
-        <li class="story-item col-md-3">
-            <div class="cover">
-                <a href="<?php echo Yii::app()->createUrl('story/view',array('slug'=>$story->story_slug))?>">
+        <li class="col-xs-6 col-md-3">
+            <div class="story-item ">
+                <a class="thumbnail" href="<?php echo Yii::app()->createUrl('story/view',array('slug'=>$story->story_slug))?>">
                     <img width="129" src="<?php echo Yii::app()->getBaseUrl(true).
                         '/public/images/'.$story->category_slug.'/'.$story->story_slug.'-md.jpg';?>" onerror="this.src='<?php echo Yii::app()->getBaseUrl(true)?>/public/images/tien-hiep/Dan-Tu-md.jpg'"/>
                 </a>
-            </div>
             <div class="info">
                 <div class="info-name">
                     <a href="<?php echo Yii::app()->createUrl('story/view',array('slug'=>$story->story_slug))?>"><h4 class="subtext"><?php echo $story->story_name?></h4></a>
                 </div>
-                <div class="info-author">
-                    <a href=""><h4 class="subtext"><?php echo (!empty($story->author))?$story->author:'Truyện Full';?></h4></a>
+                <div class="info-author text-center">
+                    <a href=""><h4 class="subtext"><span>By: </span><?php echo (!empty($story->author))?$story->author:'Truyện Full';?></h4></a>
                 </div>
-                <div class="info-category">
+                <div class="info-category text-center">
                     <a href="<?php echo Yii::app()->createUrl('home/category',array(
                         'category'=>$story->category_slug,
                         'hot' => false,
-                    ))?>"><h4 class="subtext"><?php echo $story->category_name?></h4></a>
+                    ))?>"><h4 class="subtext"><span>Thể loại: </span><?php echo $story->category_name?></h4></a>
                 </div>
-                <div class="info-category">
+                <div class="info-category text-center">
                     <a href=""><h4 class="subtext"><?php echo $story->lastest_chapter;?></h4></a>
                 </div>
             </div>
+            </div>
         </li>
         <?php endforeach;?>
+        </div>
     </ul>
     <?php else:?>
     <div class="not-found">
