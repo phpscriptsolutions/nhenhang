360135a06f8cca82f63d20d3be1bc9d0afaf64c9
tiennt <tiennt@vega.com.vn>
2015-10-31 11:30:17 +0700

    update

---
+++ protected/views/web/home/_rightMenu.php
@@ -0,0 +1,98 @@
+<div class="slide-box">
+    <div class="box-title">
+        <h2>Thể Loại Truyện</h2>
+    </div>
+    <ul>
+        <div class="row">
+            <div class="col-md-6">
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'ngon-tinh',
+                    ))?>">Ngôn Tình</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'kiem-hiep',
+                    ))?>">Kiếm Hiệp</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'tien-hiep',
+                    ))?>">Tiên Hiệp</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'xuyen-khong',
+                    ))?>">Xuyên Không</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'do-thi',
+                    ))?>">Đô Thị</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'vong-du',
+                    ))?>">Võng Du</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'huyen-huyen',
+                    ))?>">Huyền Huyễn</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'truyen-teen',
+                    ))?>">Truyện Teen</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'di-gioi',
+                    ))?>">Dị Giới</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'lich-su',
+                    ))?>">Lịch Sử</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'trong-sinh',
+                    ))?>">Trọng Sinh</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'trinh-tham',
+                    ))?>">Trinh Thám</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'khoa-huyen',
+                    ))?>">Khoa Huyền</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'di-nang',
+                    ))?>">Dị Năng</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'quan-su',
+                    ))?>">Quân Sự</a></li>
+            </div>
+            <div class="col-md-6">
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'tham-hiem',
+                    ))?>">Thám Hiểm</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'linh-di',
+                    ))?>">Linh Dị</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'sac',
+                    ))?>">Sắc</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'cung-dau',
+                    ))?>">Cung Đấu</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'nu-cuong',
+                    ))?>">Nữ Cường</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'gia-dau',
+                    ))?>">Gia Đấu</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'dong-phuong',
+                    ))?>">Đông Phương</a></li>
+
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'dam-my',
+                    ))?>">Đam Mỹ</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'bach-hop',
+                    ))?>">Bách Hợp</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'hai-huoc',
+                    ))?>">Hài Hước</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'dien-van',
+                    ))?>">Điền Văn</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'co-dai',
+                    ))?>">Cổ Đại</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl('home/category',array(
+                        'category'=>'mat-the',
+                    ))?>">Mạt Thế</a></li>
+            </div>
+        </div>
+    </ul>
+</div>
--- protected/views/web/home/index.php
+++ protected/views/web/home/index.php
@@ -28,20 +28,6 @@
         ?>
     </div>
     <div id="slide-bar" class="col-lg-3 col-md-3 hidden-sm hidden-xs">
-        <div class="slide-box">
-            <div class="box-title">
-                <h2>Thể Loại Truyện</h2>
-            </div>
-            <ul>
-                <li><a href=""><h3>Truyện Ngôn tình</h3></a></li>
-                <li><a href=""><h3>Truyện Kiếm Hiệp</h3></a></li>
-                <li><a href=""><h3>Truyện Tiên Hiệp</h3></a></li>
-                <li><a href=""><h3>Truyện Võng Du</h3></a></li>
-                <li><a href=""><h3>Truyện Trinh Thám</h3></a></li>
-                <li><a href=""><h3>Truyện Kinh Dị</h3></a></li>
-                <li><a href=""><h3>Truyện Ma</h3></a></li>
-                <li><a href=""><h3>Truyện Teen</h3></a></li>
-            </ul>
-        </div>
+        <?php include_once '_rightMenu.php';?>
     </div>
 </div>
