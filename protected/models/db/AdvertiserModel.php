f2ab99d674092fbd33cce2d450bfef9a27d28a65
tiennt <tiennt@vega.com.vn>
2015-11-17 17:12:58 +0700

    add model advertiser

---
+++ protected/models/db/AdvertiserModel.php
@@ -0,0 +1,13 @@
+<?php
+
+class AdvertiserModel extends BaseAdvertiserModel
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @return Advertiser the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
