3fa5a6f171db8f019d177b73b7993816b081c1e7
tiennt <tiennt@vega.com.vn>
2015-12-24 13:10:45 +0700

    add model advertiser

--- protected/views/web/advertiser/_listAds.php
+++ protected/views/web/advertiser/_listAds.php
@@ -1,4 +1,16 @@
 <meta charset="utf-8"/>
+<div class="gg-ads">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- ajax_detail_ads -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-7746462635786238"
+         data-ad-slot="2356319705"
+         data-ad-format="auto"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
 <?php
 $this->widget('application.widgets.web.ads.AdsWidget',
     array(
