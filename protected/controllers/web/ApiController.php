109674d9837a4e35c292ffed824b761aed75cb32
tienn2t <tienn2t@gmail.com>
2016-01-07 21:29:44 +0700

    update api

---
+++ protected/controllers/web/ApiController.php
@@ -0,0 +1,59 @@
+<?php
+class ApiController extends Controller{
+    public function actionChapter(){
+        $storyId = Yii::app()->request->getParam('story_id');
+        $limit = (int) Yii::app()->request->getParam('limit',100);
+        $offset = (int)Yii::app()->request->getParam('limit',0);
+        if(empty($storyId)){
+            exit(json_encode(array('status'=>false,'msg'=>'Invalid param','data'=>null)));
+        }
+
+        //lay thong tin truyen
+        $chapterModel = new ChapterModel();
+        $chapters = $chapterModel->getChapterByStoryId('quotev_chapter',$storyId,$limit,$offset);
+
+        if(empty($chapters)){
+            exit(json_encode(array('status'=>true,'msg'=>'Đã hết dữ liệu','data'=>null)));
+        }
+
+        $data = array();
+        foreach($chapters as $chapter){
+            $data[] = array(
+                'story_name' => $chapter['story_name'],
+                'story_id' => $chapter['story_id'],
+                'chapter_name' => $chapter['chapter_name'],
+                'story_number' => $chapter['chapter_number'],
+                'content' => $chapter['content']
+            );
+        }
+
+        exit(json_encode(array('status'=>true,'msg'=>'Success','data'=>$data)));
+    }
+
+    public function actionStory(){
+        $lastId = (int)Yii::app()->request->getParam('last_id',0);
+        $limit = (int) Yii::app()->request->getParam('limit',100);
+        $offset = (int)Yii::app()->request->getParam('limit',0);
+
+        $stories = StoryModel::model()->getStoryWithLastId($lastId,$limit,$offset,null,null,null,'id ASC');
+
+        if(empty($stories)){
+            exit(json_encode(array(
+                'status' => true,
+                'msg' => 'Empty story',
+                'data' => null
+            )));
+        }
+
+        $data = array();
+        foreach($stories as $story){
+            $data[] = array(
+                'id' => $story['id'],
+                'story_name' => $story['story_name'],
+                'description' => $story['description'],
+            );
+        }
+        exit(json_encode(array('status'=>true,'msg'=>'Success','data'=>$data)));
+
+    }
+}
--- protected/models/db/StoryModel.php
+++ protected/models/db/StoryModel.php
@@ -226,4 +226,35 @@ class StoryModel extends BaseStoryModel
 
 		return self::model()->findAll($crit);
 	}
+
+	public function getStoryWithLastId($lastId=0,$limit=12,$offset = 0,$select = null,$isHot = false,$isFull = null, $order = null){
+		$criteria = new CDbCriteria();
+
+		if(!empty($select)){
+			$criteria->select = $select;
+		}
+
+		$criteria->condition = 'id > :lastId';
+		$criteria->params = array(
+			':lastId'=> $lastId
+		);
+
+		if($isHot){
+			$criteria->addCondition('hot=1');
+		}
+		if($isFull){
+			$criteria->addCondition('status="'.$isFull.'"');
+		}
+
+		if(empty($order)){
+			$criteria->order = 'updated_time DESC';
+		}else{
+			$criteria->order = $order;
+		}
+
+		$criteria->limit = $limit;
+		$criteria->offset = $offset;
+
+		return self::model()->findAll($criteria);
+	}
 }
